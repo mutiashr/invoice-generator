c086eee4ea0091451070b4c061573493f0086ab3
mutiashr <mutiashrjh@gmail.com>
2026-04-29 11:16:59 +0700

    ubah lagi correct level

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ function generateQROnCanvas(canvas, text) {
       height: size,
       colorDark: '#000',
       colorLight: '#fff',
-      correctLevel: QRCode.CorrectLevel.H
+      correctLevel: QRCode.CorrectLevel.M
     });
     setTimeout(() => {
       const img = tmp.querySelector('img') || tmp.querySelector('canvas');
